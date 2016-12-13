42930e0b8e95e1c2cfcbe541dfbdaa9065a6734e
StanleyZhao <stanley.c.zhao@gmail.com>
2016-12-13 16:45:35 -0500

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+$(document).ready( function(){
+
+ var oldsummary1 = window.summary1;
+    window.summary1 = function() {
+    window.alert('hello overwritten');
+    console.log("test for override");
+    oldsummary1();
+  };
+});
