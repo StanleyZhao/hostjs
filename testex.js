9a08acdbb0811c8cf7c00444e0a2cbb990066cb0
StanleyZhao <stanley.c.zhao@gmail.com>
2016-12-13 17:29:46 -0500

    Update testex.js

--- testex.js
+++ testex.js
@@ -1,10 +1,12 @@
 $(document).ready( function(){
 console.log("test if get in");
-function summary1() {
+$("#continue_button_1").on("click", function(){
         $("#email_1_summary").text($("#email_1").val());
         // $("#loan_amount_1_summary").text($("#loan_amount_1").find("option:selected").text());
         $("#loan_amount_1_summary").text($("#loan_amount_1").val());
         $("#loan_purpose_summary").text($("#primary_radio_1 .radio_label_2.ischecked").find("span").text());
- console.log("test if override");
-    }
+        console.log("test if overwrite");
+        gaEvent_LoanDetails_Continue();
+
+    });
 });
