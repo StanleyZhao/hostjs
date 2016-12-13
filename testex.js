c4d40672298b6c6b8208ddbd6bcb68fb1ea8c874
StanleyZhao <stanley.c.zhao@gmail.com>
2016-12-13 17:03:00 -0500

    Update testex.js

--- testex.js
+++ testex.js
@@ -1,9 +1,8 @@
-$(document).ready( function(){
 
  var oldsummary1 = window.summary1;
     window.summary1 = function() {
     window.alert('hello overwritten');
     console.log("test for override");
     oldsummary1();
+
   };
-});
